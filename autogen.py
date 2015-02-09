f5bf9fd39fce1b240f714821c2718fa629e543f1
Friedrich W. H. Kossebau <kossebau@kde.org>
2015-02-09 10:55:06 +0100

    Remove unused autogen.py

--- autogen.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from autogen.autogen import autogen
-
-project = "KDiagram"
-version = "2.6.0"
-subprojects = ["KChart", "KGantt"]
-prefixed = True
-
-autogen(project, version, subprojects, prefixed, policyVersion = 2)

a470d77be5a33529a95eb89243ddfaef6ee27ee7
Yuri Chornoivan <yurchor@ukr.net>
2019-01-06 20:37:57 +0200

    Fix minor Doxygen issues

--- src/KChart/KChartGlobal.h
+++ src/KChart/KChartGlobal.h
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/** \file KChartGlobal
+/** \file KChartGlobal.h
 \brief Contains KChart macros.
 
 Contains KChart macros.  */

KDE/kdiagram

commit 2608d080dce1c652b0726f790e9cac320b8b5241

Fix mem leak found by ASAN

src/KGantt/kganttgraphicsscene.cpp
@@ -71,6 +71,11 @@ GraphicsScene::Private::Private( GraphicsScene* _q )
     default_grid.setStartDateTime( QDateTime::currentDateTime().addDays( -1 ) );
 }
 
+GraphicsScene::Private::~Private()
+{
+    delete grid;
+}
+
 void GraphicsScene::Private::clearConstraintItems()
 {
     for(ConstraintGraphicsItem *citem : constraintItems) {
@@ -322,6 +327,7 @@ void GraphicsScene::setGrid( AbstractGrid* grid )
         d->getGrid()->disconnect( this );
         model = d->getGrid()->model();
     }
+    delete d->grid;
     d->grid = grid;
     connect( d->getGrid(), SIGNAL(gridChanged()), this, SLOT(slotGridChanged()) );
     d->getGrid()->setModel( model );

src/KGantt/kganttgraphicsview.cpp
@@ -205,6 +205,10 @@ GraphicsView::Private::Private( GraphicsView* _q )
 {
 }
 
+GraphicsView::Private::~Private()
+{
+}
+
 void GraphicsView::Private::updateHeaderGeometry()
 {
     q->setViewportMargins(0,rowcontroller->headerHeight(),0,0);
@@ -343,7 +347,6 @@ void GraphicsView::Private::slotHeaderContextMenuRequested( const QPoint& pt )
 GraphicsView::GraphicsView( QWidget* parent )
     : QGraphicsView( parent ), _d( new Private( this ) )
 {
-
 #if defined KDAB_EVAL
   EvalDialog::checkEvalLicense( "KD Gantt" );
 #endif

src/KGantt/kganttgraphicsscene_p.h
@@ -36,6 +36,7 @@ namespace KGantt {
     class Q_DECL_HIDDEN GraphicsScene::Private {
     public:
         explicit Private(GraphicsScene*);
+        ~Private();
 
         void clearConstraintItems();
         void resetConstraintItems();

src/KGantt/kganttgraphicsview_p.h
@@ -49,6 +49,7 @@ namespace KGantt {
         Q_DISABLE_COPY( Private )
     public:
         explicit Private(GraphicsView* _q);
+        ~Private();
 
         void updateHeaderGeometry();
 

commit 2d7fa680b441dcff827288b3e1fd370e1146f3d1

KGantt: Fix crash introduced by 2608d080dce1c652b0726f790e9cac320b8b5241
The crash occured when a grid was set on one graphics view and then moved
to a different view, since the grid is deleted when taken from a view.
This patch introduces a separate takeGrid() to enable taking a grid wo deleting it.

src/KGantt/kganttgraphicsscene.cpp
@@ -320,6 +320,18 @@ AbstractRowController* GraphicsScene::rowController() const
     return d->rowController;
 }
 
+AbstractGrid *GraphicsScene::takeGrid()
+{
+    AbstractGrid *grid = d->grid;
+    grid->disconnect( this );
+    d->grid = nullptr;
+    if (grid) {
+        // revert to the default_grid
+        connect( &d->default_grid, SIGNAL(gridChanged()), this, SLOT(slotGridChanged()) );
+    }
+    return grid;
+}
+
 void GraphicsScene::setGrid( AbstractGrid* grid )
 {
     QAbstractItemModel* model = nullptr;

src/KGantt/kganttgraphicsscene.h
@@ -93,9 +93,27 @@ namespace KGantt {
         void setRowController( AbstractRowController* rc );
         AbstractRowController* rowController() const;
 
+        /**
+         * Set the grid to @p grid.
+         * The current grid (if set) is deleted.
+         * If @p grid is nullptr, the scene reverts to use the default_grid
+         */
         void setGrid( AbstractGrid* grid );
+        /**
+         * @return the grid set with setGrid().
+         * Note: Returns nullptr if no grid has been set.
+         */
         AbstractGrid* grid() const;
+        /**
+         * @return the current grid.
+         */
         const AbstractGrid *getGrid() const;
+        /**
+         * @return the grid set with setGrid()
+         * Note: Returns nullptr if no grid has been set.
+         * The scene reverts to use the default_grid.
+         */
+        AbstractGrid *takeGrid();
 
         bool isReadOnly() const;
 

src/KGantt/kganttgraphicsview.cpp
@@ -519,11 +519,7 @@ AbstractGrid* GraphicsView::grid() const
 
 AbstractGrid* GraphicsView::takeGrid()
 {
-    AbstractGrid *grid = d->scene.grid();
-    if ( grid ) {
-        d->scene.setGrid( nullptr) ;
-    }
-    return grid;
+    return d->scene.takeGrid();
 }
 
 

src/KGantt/kganttview.cpp
@@ -324,10 +324,11 @@ void View::setGraphicsView( GraphicsView* gv )
 {
     if ( gv != d->gfxview ) {
         GraphicsView* old = d->gfxview;
+        AbstractGrid *grid = old->takeGrid();
         d->gfxview = gv;
         d->gfxview->setModel(old->model()); // use the old ForwardingProxyModel
         d->setupGraphicsView();
-        d->gfxview->setGrid( old->takeGrid() );
+        d->gfxview->setGrid( grid );
         delete old;
     }
 }